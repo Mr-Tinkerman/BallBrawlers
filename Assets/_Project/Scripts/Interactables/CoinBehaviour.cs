47463a17ff02800bc3cf4b42a8b4019f2de3531e
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-11 15:53:07 -0600

    Made coin script flexible, Prefabs handle coin val

--- Assets/_Project/Scripts/Interactables/CoinBehaviour.cs
+++ Assets/_Project/Scripts/Interactables/CoinBehaviour.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinBehaviour : MonoBehaviour
 {
-    public Coin coin;
+    public int coinValue = 1;
     public SaveManager sm;
 
     void Awake()
@@ -14,29 +12,9 @@ public class CoinBehaviour : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        sm.AddCoins(coin.Value());
+        sm.AddCoins(coinValue);
         sm.Save();
 
         Destroy(this.gameObject);
     }
-}
-
-[System.Serializable]
-public class Coin
-{
-    public CoinType coinType;
-
-    static int[] CoinValue = { 1, 10 };
-    
-    public int Value()
-    {
-        return CoinValue[((int)coinType)];
-    }
-}
-
-[System.Serializable]
-public enum CoinType
-{
-    Coin = 0,
-    Pouch = 1
 }
