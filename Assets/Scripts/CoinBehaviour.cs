aa4ad11602f36218d27a72525a5662e8a116b51e
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-11 00:35:59 -0600

    Coins now add to a score and delete themselves

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CoinBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Coin coin;
+    public SaveManager sm;
+
+    void Awake()
     {
-        
+        sm = GameObject.FindObjectOfType<SaveManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter()
     {
-        
+        sm.AddCoins(coin.Value());
+        sm.Save();
+
+        Destroy(this.gameObject);
     }
+}
+
+[System.Serializable]
+public class Coin
+{
+    public CoinType coinType;
+
+    static int[] CoinValue = { 1, 10 };
+    
+    public int Value()
+    {
+        return CoinValue[((int)coinType)];
+    }
+}
+
+[System.Serializable]
+public enum CoinType
+{
+    Coin = 0,
+    Pouch = 1
 }
