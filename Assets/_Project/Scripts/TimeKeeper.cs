206207c7354ea1a657e531beb738f10eb18f2ef1
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-11 22:51:40 -0600

    Keeps track of time and ends game on time out

---
+++ Assets/_Project/Scripts/TimeKeeper.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class TimeKeeper : MonoBehaviour
+{
+    private float startingTime = 30;
+    private float timeLeftSeconds;
+
+    public static TimeKeeper Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        ResetTime();
+    }
+
+    void Update()
+    {
+        // Game timer countdown
+        if (timeLeftSeconds > 0)
+        {
+            timeLeftSeconds -= Time.deltaTime;
+            timeLeftSeconds = Mathf.Max(timeLeftSeconds, 0);
+        }
+        else
+        {
+            GameStateManager.Instance.GameOver();
+        }
+    }
+
+    public void AddTime(int seconds)
+    {
+        timeLeftSeconds += seconds;
+    }
+
+    public void RemoveTime(int seconds)
+    {
+        timeLeftSeconds -= seconds;
+        timeLeftSeconds = Mathf.Max(timeLeftSeconds, 0);
+    }
+
+    public float GetTime()
+    {
+        return timeLeftSeconds;
+    }
+
+    public void ResetTime()
+    {
+        timeLeftSeconds = startingTime;
+    }
+}
