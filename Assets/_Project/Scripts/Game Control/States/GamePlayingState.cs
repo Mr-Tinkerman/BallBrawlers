e6ee51e24ee6b96dc48af0306aa70a4a363a4992
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-20 17:05:02 -0600

    Started to add functionality to play state

--- Assets/_Project/Scripts/Game Control/States/GamePlayingState.cs
+++ Assets/_Project/Scripts/Game Control/States/GamePlayingState.cs
@@ -1,4 +1,22 @@
+using UnityEngine;
+
 public class GamePlayingState : GameStateBase
 {
-    
+    public Timer gameTimer = new Timer(30);
+
+    public override void Enter()
+    {
+        gameTimer.OnTimeDepleted += OnGameTimerEnd;
+        gameTimer.Start();
+    }
+
+    public override void Exit()
+    {
+        gameTimer.OnTimeDepleted -= OnGameTimerEnd;
+    }
+
+    private void OnGameTimerEnd()
+    {
+        GameManager.Instance.SwitchState<GameOverState>();
+    }
 }
