ff58ddf65086776eae20af107e787259f6e2e6eb
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-20 15:58:54 -0600

    Renamed class to show correlation with game base

--- Assets/_Project/Scripts/Game Control/States/GameMenuState.cs
+++ Assets/_Project/Scripts/Game Control/States/GameMenuState.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MenuState : GameStateBase
+public class GameMenuState : GameStateBase
 {
     
 }
--- Assets/_Project/Scripts/Game Control/States/GamePlayingState.cs
+++ Assets/_Project/Scripts/Game Control/States/GamePlayingState.cs
@@ -1,4 +1,4 @@
-public class PlayingState : GameStateBase
+public class GamePlayingState : GameStateBase
 {
     
 }
