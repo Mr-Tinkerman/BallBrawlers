3aab9d4df97f45d354559bb8ce20248d320d4aed
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-20 04:24:59 -0600

    Made UI manager a singleton

--- Assets/_Project/Scripts/UI/UIManager.cs
+++ Assets/_Project/Scripts/UI/UIManager.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    //
+    [SerializeField]
+    private List<Canvas> UILayers;
+
+    public static UIManager Instance { get; private set; }
+
+    void Awake ()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
 }
