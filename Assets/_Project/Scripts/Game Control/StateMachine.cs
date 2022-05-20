5b11ef5e7a8460c80392c63841aa662bb574b4a1
Mr-Tinkerman <tinkermangames@gmail.com>
2022-05-20 15:47:53 -0600

    Removed unused usings

--- Assets/_Project/Scripts/Game Control/StateMachine.cs
+++ Assets/_Project/Scripts/Game Control/StateMachine.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using UnityEngine;
-
 public class StateMachine
 {
     IState currentState;
