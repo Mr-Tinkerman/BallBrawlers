a242d16d0c86f030095ea579999dfd6b8a157276
Mr-Tinkerman <tinkermangames@gmail.com>
2022-06-23 01:50:40 -0600

    Switches state on play clicked, Rename apply

--- Assets/_Project/Scripts/Game Control/States/UIMainMenuState.cs
+++ Assets/_Project/Scripts/Game Control/States/UIMainMenuState.cs
@@ -2,13 +2,15 @@ public class UIMainMenuState : UIStateBase
 {
     public void HandlePlayButtonClick()
     {
-        GameManager.GetGameState<GamePlayingState>().ResetTimer();
+        GameManager.GetGameState<GamePlayingState>().ResetGame();
         GameManager.Instance.SwitchState<GamePlayingState>();
+        
+        UIManager.Instance.SwitchState<UIHUDState>();
     }
 
     public void HandleSkinsButtonClick()
     {
-        UIManager.Instance.SwitchState<UIStoreMenuState>();
+        UIManager.Instance.SwitchState<UISkinsShopState>();
     }
 
     // public void HandleAboutButtonClick()
