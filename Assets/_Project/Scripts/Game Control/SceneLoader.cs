335b3283dc81e01ee38df5c4b988b26accd8857a
Mr-Tinkerman <tinkermangames@gmail.com>
2022-07-04 14:05:14 -0600

    Loads scenes, force loads without a game manager

---
+++ Assets/_Project/Scripts/Game Control/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+#if !UNITY_EDITOR
+    void Awake()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+#endif
+}
